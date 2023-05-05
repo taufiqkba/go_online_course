90924c789147c3dcc57adeb64ddb4c2bcfb6af19
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-05 23:37:39 +0700

    Create utils/helper randNumber and httpResponse

---
+++ pkg/utils/utils.go
@@ -0,0 +1,13 @@
+package utils
+
+import "math/rand"
+
+func RandomString(number int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+	b := make([]rune, number)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
