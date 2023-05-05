671feb80a4db699463ccca28f4029321baba3d11
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-05 21:18:26 +0700

    Create db connection

--- pkg/db/mysql/db_conn.go
+++ pkg/db/mysql/db_conn.go
@@ -1,1 +1,30 @@
 package mysql
+
+import (
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+func DB() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Can't load file .env")
+	}
+
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	username := os.Getenv("MYSQL_USERNAME")
+	password := os.Getenv("MYSQL_PASSWORD")
+
+	dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname + "?charset=utf8&parseTime=true&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Can't connect to database")
+	}
+
+	return db
+}
