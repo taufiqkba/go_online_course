eb1825bdb78f4f0fad79d56546d8a4f0fe484030
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-05 23:37:56 +0700

    Setup main.go main function

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"go_online_course/internal/register/delivery/http"
+	usecase2 "go_online_course/internal/register/usecase"
+	"go_online_course/internal/user/repository"
+	"go_online_course/internal/user/usecase"
+	"go_online_course/pkg/db/mysql"
 )
 
 func main() {
+	db := mysql.DB()
 	r := gin.Default()
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "test create api",
-		})
-	})
+	userRepository := repository.NewUserRepositoryImpl(db)
+	userUseCase := usecase.NewUserUseCase(userRepository)
+	registerUseCase := usecase2.NewRegisterUseCase(userUseCase)
+	http.NewRegisterHandler(registerUseCase).Route(&r.RouterGroup)
 
 	r.Run() //0.0.0.0:8080
 }
