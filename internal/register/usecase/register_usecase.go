392a5d48bd6f05df79e7cf02c2dc8e64c27b000c
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-08 19:10:27 +0700

    fix

--- internal/register/usecase/register_usecase.go
+++ internal/register/usecase/register_usecase.go
@@ -1,8 +1,10 @@
 package usecase
 
 import (
+	register "go_online_course/internal/register/dto"
 	"go_online_course/internal/user/dto"
 	"go_online_course/internal/user/usecase"
+	"go_online_course/pkg/mail/sendgrid"
 )
 
 type RegisterUseCase interface {
@@ -11,19 +13,26 @@ type RegisterUseCase interface {
 
 type RegisterUseCaseImpl struct {
 	userUseCase usecase.UserUseCase
+	mail        sendgrid.Mail
 }
 
-func NewRegisterUseCase(userUseCase usecase.UserUseCase) RegisterUseCase {
-	return &RegisterUseCaseImpl{userUseCase}
+func NewRegisterUseCase(userUseCase usecase.UserUseCase, mail sendgrid.Mail) RegisterUseCase {
+	return &RegisterUseCaseImpl{userUseCase, mail}
 }
 
 func (ru *RegisterUseCaseImpl) Register(userDto dto.UserRequestBody) error {
 	//	check into user module
-	_, err := ru.userUseCase.Create(userDto)
+	user, err := ru.userUseCase.Create(userDto)
 	if err != nil {
 		return err
 	}
 	//	send email
+	email := register.CrateEmailVerification{
+		Subject:          "Verification Code",
+		Email:            user.Email,
+		VerificationCode: user.CodeVerified,
+	}
+	ru.mail.SendVerificationEmail(user.Email, email)
 
 	return nil
 }
