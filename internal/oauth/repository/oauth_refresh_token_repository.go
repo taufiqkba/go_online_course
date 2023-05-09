5d367f5cdc08cbb25035945c2406867e352b7b5b
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-09 12:02:28 +0700

    Complete and refactor oauth_refresh_token repository

--- internal/oauth/repository/oauth_refresh_token_repository.go
+++ internal/oauth/repository/oauth_refresh_token_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"go_online_course/internal/oauth/entity"
+
 	"gorm.io/gorm"
 )
 
@@ -12,31 +13,36 @@ type OauthRefreshTokenRepository interface {
 }
 
 type OauthRefreshTokenRepositoryImpl struct {
+	//connect to database
 	db *gorm.DB
 }
 
+// Create implements OauthRefreshTokenRepository
 func (repository *OauthRefreshTokenRepositoryImpl) Create(oauthRefreshToken entity.OauthRefreshToken) (*entity.OauthRefreshToken, error) {
-	if err := repository.db.Create(oauthRefreshToken).Error; err != nil {
+	if err := repository.db.Create(&oauthRefreshToken).Error; err != nil {
 		return nil, err
 	}
 	return &oauthRefreshToken, nil
 }
 
-func (repository *OauthRefreshTokenRepositoryImpl) FindOneByToken(token string) (*entity.OauthRefreshToken, error) {
-	//var oauthRefreshToken entity.OauthRefreshToken
-
-	//if err := repository.db.Where("token = ?", token)
-	// TODO Implement me
-	panic("implement me!")
-}
-
+// Delete implements OauthRefreshTokenRepository
 func (repository *OauthRefreshTokenRepositoryImpl) Delete(id int) error {
-	var oauthRefreshToken entity.OauthRefreshToken
+	var oauthRefreshToken entity.OauthAccessToken
 
-	if err := repository.db.Delete(&oauthRefreshToken, id).Error; err != nil {
-		return err
+	if result := repository.db.Delete(&oauthRefreshToken, id).Error; result != nil {
+		return result
 	}
 	return nil
+}
+
+// FindOneByToken implements OauthRefreshTokenRepository
+func (repository *OauthRefreshTokenRepositoryImpl) FindOneByToken(token string) (*entity.OauthRefreshToken, error) {
+	var oauthRefreshToken entity.OauthRefreshToken
+
+	if err := repository.db.Where("token = ?", token).First(&oauthRefreshToken).Error; err != nil {
+		return nil, err
+	}
+	return &oauthRefreshToken, nil
 }
 
 func NewOauthRefreshTokenRepositoryRepository(db *gorm.DB) OauthRefreshTokenRepository {
