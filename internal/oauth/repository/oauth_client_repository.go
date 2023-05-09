14baf96c488d57ea32b381dffce496f4815887a4
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-09 11:37:50 +0700

    Create oauth_client_repository

--- internal/oauth/repository/oauth_client_repository.go
+++ internal/oauth/repository/oauth_client_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"go_online_course/internal/oauth/entity"
+
 	"gorm.io/gorm"
 )
 
@@ -9,11 +10,13 @@ type OauthClientRepository interface {
 	FindByClientIdAndClientSecret(clientId string, clientSecret string) (*entity.OauthClient, error)
 }
 
-type OauthClientImpl struct {
+type OauthClientRepositoryImpl struct {
+	//connect to database
 	db *gorm.DB
 }
 
-func (oc *OauthClientImpl) FindByClientIdAndClientSecret(clientId string, clientSecret string) (*entity.OauthClient, error) {
+// FindByClientIdAndClientSecret implements OauthClientRepository
+func (oc *OauthClientRepositoryImpl) FindByClientIdAndClientSecret(clientId string, clientSecret string) (*entity.OauthClient, error) {
 	var oauthClient entity.OauthClient
 
 	if err := oc.db.Where("client_id = ?", clientId).Where("client_secret = ?", clientSecret).First(&oauthClient).Error; err != nil {
@@ -23,6 +26,6 @@ func (oc *OauthClientImpl) FindByClientIdAndClientSecret(clientId string, client
 	return &oauthClient, nil
 }
 
-func NewOauthClientRepository() OauthClientRepository {
-	return &OauthClientImpl{}
+func NewOauthClientRepository(db *gorm.DB) OauthClientRepository {
+	return &OauthClientRepositoryImpl{db}
 }
