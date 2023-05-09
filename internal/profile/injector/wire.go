b588ab0c1870c41b8cde886e465f550ba6a9fa92
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-09 19:56:46 +0700

    Generate wire for get_profile service

--- internal/profile/injector/wire.go
+++ internal/profile/injector/wire.go
@@ -1,1 +1,24 @@
+//go:build wireinject
+// +build wireinject
+
 package injector
+
+import (
+	"github.com/google/wire"
+	"go_online_course/internal/profile/delivery/http"
+	"go_online_course/internal/profile/usecase"
+	"go_online_course/internal/user/repository"
+	usecase2 "go_online_course/internal/user/usecase"
+	"gorm.io/gorm"
+)
+
+func InitializedService(db *gorm.DB) *http.ProfileHandler {
+	wire.Build(
+		http.NewProfileHandler,
+		usecase.NewProfileUseCase,
+		repository.NewUserRepositoryImpl,
+		usecase2.NewUserUseCase,
+	)
+
+	return &http.ProfileHandler{}
+}
