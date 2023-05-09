baeb2a68a6c8e6e116e7b9b341e1622090dc0161
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-09 11:35:56 +0700

    Fix several data type for oauth_response

--- internal/oauth/dto/oauth_response.go
+++ internal/oauth/dto/oauth_response.go
@@ -5,9 +5,9 @@ import "github.com/golang-jwt/jwt/v5"
 type LoginResponse struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
-	Type         string `json:"type"` //using bearer_token
-	ExpiredAt    string `json:"expired_at"`
-	Scope        string `json:"scope"`
+	Type         string `json:"type"`       //using bearer_token
+	ExpiredAt    string `json:"expired_at"` //expired for access_token
+	Scope        string `json:"scope"`      // token available what that can do?
 }
 
 type UserResponse struct {
@@ -21,14 +21,14 @@ type ClaimResponse struct {
 	ID      int64  `json:"id"`
 	Name    string `json:"name"`
 	Email   string `json:"email"`
-	IsAdmin bool   `json:"is_admin",omitempty`
+	IsAdmin bool   `json:"is_admin,omitempty"`
 	jwt.RegisteredClaims
 }
 
 type MapClaimResponse struct {
-	ID      int64  `json:"id"`
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	IsAdmin bool   `json:"is_admin"`
-	jwt.RegisteredClaims
+	ID            int64  `json:"id"`
+	Name          string `json:"name"`
+	Email         string `json:"email"`
+	IsAdmin       bool   `json:"is_admin"`
+	jwt.MapClaims `json:"omitempty"`
 }
