12dcc75c1d16da5cd97e12583b8523a4069dd8e0
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-10 15:23:20 +0700

    Implement usecase method FindByEmail

--- internal/admin/usecase/admin_usecase.go
+++ internal/admin/usecase/admin_usecase.go
@@ -4,15 +4,14 @@ import (
 	"go_online_course/internal/admin/dto"
 	"go_online_course/internal/admin/entity"
 	"go_online_course/internal/admin/repository"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type AdminUseCase interface {
 	FindAll(offset int, limit int) []entity.Admin
-	FindById(id int) (*entity.Admin, error)
+	FindByID(id int) (*entity.Admin, error)
 	FindByEmail(email string) (*entity.Admin, error)
-	Create(dto dto.AdminRequestBody) (*entity.Admin, error)
-	Update(id int, dto dto.AdminRequestBody) (*entity.Admin, error)
+	Create(dto dto.AdminRequestBody) (*entity.Admin, error) //why depend to AdminRequestBody
+	Update(dto dto.AdminRequestBody) (*entity.Admin, error)
 	Delete(id int) error
 }
 
@@ -22,20 +21,7 @@ type AdminUseCaseImpl struct {
 
 // Create implements AdminUseCase
 func (usecase *AdminUseCaseImpl) Create(dto dto.AdminRequestBody) (*entity.Admin, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(dto.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return nil, err
-	}
-
-	dataAdmin := entity.Admin{
-		Name:        dto.Name,
-		Email:       dto.Email,
-		Password:    dto.Password,
-		CreatedById: dto.CreatedBy,
-	}
-
-	admin, err := usecase.repository.Create(dataAdmin)
-
+	panic("unimplemented")
 }
 
 // Delete implements AdminUseCase
@@ -53,16 +39,76 @@ func (usecase *AdminUseCaseImpl) FindByEmail(email string) (*entity.Admin, error
 	return usecase.repository.FindByEmail(email)
 }
 
-// FindById implements AdminUseCase
-func (usecase *AdminUseCaseImpl) FindById(id int) (*entity.Admin, error) {
-	return usecase.repository.FindById(id)
+// FindByID implements AdminUseCase
+func (usecase *AdminUseCaseImpl) FindByID(id int) (*entity.Admin, error) {
+	panic("unimplemented")
 }
 
 // Update implements AdminUseCase
-func (usecase *AdminUseCaseImpl) Update(id int, dto dto.AdminRequestBody) (*entity.Admin, error) {
+func (usecase *AdminUseCaseImpl) Update(dto dto.AdminRequestBody) (*entity.Admin, error) {
 	panic("unimplemented")
 }
 
 func NewAdminUseCase(repository repository.AdminRepository) AdminUseCase {
 	return &AdminUseCaseImpl{repository}
 }
+
+// type AdminUseCase interface {
+// 	FindAll(offset int, limit int) []entity.Admin
+// 	FindById(id int) (*entity.Admin, error)
+// 	FindByEmail(email string) (*entity.Admin, error)
+// 	Create(dto dto.AdminRequestBody) (*entity.Admin, error)
+// 	Update(id int, dto dto.AdminRequestBody) (*entity.Admin, error)
+// 	Delete(id int) error
+// }
+
+// type AdminUseCaseImpl struct {
+// 	repository repository.AdminRepository
+// }
+
+// // Create implements AdminUseCase
+// func (usecase *AdminUseCaseImpl) Create(dto dto.AdminRequestBody) (*entity.Admin, error) {
+// 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(dto.Password), bcrypt.DefaultCost)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	dataAdmin := entity.Admin{
+// 		Name:        dto.Name,
+// 		Email:       dto.Email,
+// 		Password:    dto.Password,
+// 		CreatedById: dto.CreatedBy,
+// 	}
+
+// 	admin, err := usecase.repository.Create(dataAdmin)
+
+// }
+
+// // Delete implements AdminUseCase
+// func (usecase *AdminUseCaseImpl) Delete(id int) error {
+// 	panic("unimplemented")
+// }
+
+// // FindAll implements AdminUseCase
+// func (usecase *AdminUseCaseImpl) FindAll(offset int, limit int) []entity.Admin {
+// 	panic("unimplemented")
+// }
+
+// // FindByEmail implements AdminUseCase
+// func (usecase *AdminUseCaseImpl) FindByEmail(email string) (*entity.Admin, error) {
+// 	return usecase.repository.FindByEmail(email)
+// }
+
+// // FindById implements AdminUseCase
+// func (usecase *AdminUseCaseImpl) FindById(id int) (*entity.Admin, error) {
+// 	return usecase.repository.FindById(id)
+// }
+
+// // Update implements AdminUseCase
+// func (usecase *AdminUseCaseImpl) Update(id int, dto dto.AdminRequestBody) (*entity.Admin, error) {
+// 	panic("unimplemented")
+// }
+
+// func NewAdminUseCase(repository repository.AdminRepository) AdminUseCase {
+// 	return &AdminUseCaseImpl{repository}
+// }
