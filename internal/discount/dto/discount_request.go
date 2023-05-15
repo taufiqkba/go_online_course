8a950fb2467c87008a273210d66522947640a2df
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-15 12:01:14 +0700

    change data type for startDate and endDate field

--- internal/discount/dto/discount_request.go
+++ internal/discount/dto/discount_request.go
@@ -1,14 +1,16 @@
 package dto
 
+import "time"
+
 type DiscountRequestBody struct {
-	Name              string `json:"name" binding:"required"`
-	Code              string `json:"code" binding:"required"`
-	Quantity          int64  `json:"quantity" binding:"required"`
-	Type              string `json:"type" binding:"required"`
-	RemainingQuantity int64  `json:"remaining_quantity" binding:"number"`
-	Value             int64  `json:"value" binding:"required,number"`
-	StartDate         string `json:"start_date"`
-	EndDate           string `json:"end_date"`
-	CreatedBy         int64  `json:"created_by"`
-	UpdatedBy         int64  `json:"updated_by"`
+	Name              string    `json:"name" binding:"required"`
+	Code              string    `json:"code" binding:"required"`
+	Quantity          int64     `json:"quantity" binding:"required"`
+	Type              string    `json:"type" binding:"required"`
+	RemainingQuantity int64     `json:"remaining_quantity" binding:"number"`
+	Value             int64     `json:"value" binding:"required,number"`
+	StartDate         time.Time `json:"start_date"`
+	EndDate           time.Time `json:"end_date"`
+	CreatedBy         int64     `json:"created_by"`
+	UpdatedBy         int64     `json:"updated_by"`
 }
