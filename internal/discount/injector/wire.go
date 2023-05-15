9a0f731955770e46dfdb5a08949660923de3c99e
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-15 12:50:07 +0700

    implement injector for discount service

--- internal/discount/injector/wire.go
+++ internal/discount/injector/wire.go
@@ -1,1 +1,21 @@
+//go:build wireinject
+// +build wireinject
+
 package injector
+
+import (
+	"github.com/google/wire"
+	"go_online_course/internal/discount/delivery/http"
+	"go_online_course/internal/discount/repository"
+	"go_online_course/internal/discount/usecase"
+	"gorm.io/gorm"
+)
+
+func InitializeService(db *gorm.DB) *http.DiscountHandler {
+	wire.Build(
+		http.NewDiscountHandler,
+		repository.NewDiscountRepository,
+		usecase.NewDiscountUseCase,
+	)
+	return &http.DiscountHandler{}
+}
