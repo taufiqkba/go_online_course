a92b458a3f09ac57b8fba9a9f89c04e153b7c0a6
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-15 22:38:24 +0700

    Create payment_usecase

--- internal/payment/usecase/payment_usecase.go
+++ internal/payment/usecase/payment_usecase.go
@@ -1,11 +1,47 @@
 package usecase
 
+import (
+	"github.com/xendit/xendit-go"
+	"github.com/xendit/xendit-go/invoice"
+	"go_online_course/internal/payment/dto"
+	"os"
+)
+
 type PaymentUseCase interface {
+	Create(dto dto.PaymentRequestBody) (*xendit.Invoice, error)
 }
 
 type PaymentUseCaseImpl struct {
 }
 
+func (useCase *PaymentUseCaseImpl) Create(dto dto.PaymentRequestBody) (*xendit.Invoice, error) {
+	data := invoice.CreateParams{
+		ExternalID:  dto.ExternalID,
+		Amount:      float64(dto.Amount),
+		Description: dto.Description,
+		PayerEmail:  dto.PayerEmail,
+		Customer: xendit.InvoiceCustomer{
+			Email: dto.PayerEmail,
+		},
+		CustomerNotificationPreference: xendit.InvoiceCustomerNotificationPreference{
+			InvoiceCreated:  []string{"email"},
+			InvoiceReminder: []string{"email"},
+			InvoicePaid:     []string{"email"},
+			InvoiceExpired:  []string{"email"},
+		},
+		InvoiceDuration:    86400,
+		SuccessRedirectURL: os.Getenv("XENDIT_SUCCESS_URL"),
+	}
+
+	resp, err := invoice.Create(&data)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
 func NewPaymentUseCase() PaymentUseCase {
+	//Setup Xendit PaymentGateway
+	xendit.Opt.SecretKey = os.Getenv("XENDIT_KEY")
 	return &PaymentUseCaseImpl{}
 }
