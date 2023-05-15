47de2d1b6fda8096ac97d080d7ae278953f9a6a9
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-15 09:48:48 +0700

    Create discount_request

--- internal/discount/dto/discount_request.go
+++ internal/discount/dto/discount_request.go
@@ -1,1 +1,14 @@
 package dto
+
+type DiscountRequestBody struct {
+	Name              string `json:"name" binding:"required"`
+	Code              string `json:"code" binding:"required"`
+	Quantity          int64  `json:"quantity" binding:"required"`
+	Type              string `json:"type" binding:"required"`
+	RemainingQuantity int64  `json:"remaining_quantity" binding:"number"`
+	Value             int64  `json:"value" binding:"required,number"`
+	StartDate         string `json:"start_date"`
+	EndDate           string `json:"end_date"`
+	CreatedBy         int64  `json:"created_by"`
+	UpdatedBy         int64  `json:"updated_by"`
+}
