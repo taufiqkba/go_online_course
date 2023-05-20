c8966f12d3f576244214851b15dc0bec606dc3d1
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-21 00:11:59 +0700

    Complete class_room repository

--- internal/class_room/repository/class_room_repository.go
+++ internal/class_room/repository/class_room_repository.go
@@ -3,12 +3,13 @@ package repository
 import (
 	entity4 "go_online_course/internal/class_room/entity"
 	"go_online_course/pkg/utils"
+
 	"gorm.io/gorm"
 )
 
 type ClassRoomRepository interface {
 	FindAllByUserID(offset int, limit int, userID int) []entity4.ClassRoom
-	FindOneByUserIDAndProductID(userID int, productID int) (*entity4.ClassRoom, error)
+	FindByOneByUserIDAndProductID(userID int, productID int) (*entity4.ClassRoom, error)
 	Create(entity entity4.ClassRoom) (*entity4.ClassRoom, error)
 }
 
@@ -16,29 +17,32 @@ type ClassRoomRepositoryImpl struct {
 	db *gorm.DB
 }
 
+// Create implements ClassRoomRepository
+func (repository *ClassRoomRepositoryImpl) Create(entity entity4.ClassRoom) (*entity4.ClassRoom, error) {
+	err := repository.db.Create(&entity).Error
+	if err != nil {
+		return nil, err
+	}
+	return &entity, nil
+}
+
+// FindAllByUserID implements ClassRoomRepository
 func (repository *ClassRoomRepositoryImpl) FindAllByUserID(offset int, limit int, userID int) []entity4.ClassRoom {
 	var classRoom []entity4.ClassRoom
 
 	repository.db.Scopes(utils.Paginate(offset, limit)).Preload("Product.ProductCategory").Where("user_id = ?", userID).Find(&classRoom)
-
 	return classRoom
 }
 
-func (repository *ClassRoomRepositoryImpl) FindOneByUserIDAndProductID(userID int, productID int) (*entity4.ClassRoom, error) {
+// FindByOneByUserIDAndProductID implements ClassRoomRepository
+func (repository *ClassRoomRepositoryImpl) FindByOneByUserIDAndProductID(userID int, productID int) (*entity4.ClassRoom, error) {
 	var classRoom entity4.ClassRoom
 
-	if err := repository.db.Where("user_id = ?", userID).Where("product_id", productID).First(&classRoom).Error; err != nil {
+	err := repository.db.Where("user_id = ?", userID).Where("product_id", productID).First(&classRoom).Error
+	if err != nil {
 		return nil, err
 	}
 	return &classRoom, nil
-}
-
-func (repository *ClassRoomRepositoryImpl) Create(entity entity4.ClassRoom) (*entity4.ClassRoom, error) {
-	if err := repository.db.Create(&entity).Error; err != nil {
-		return nil, err
-	}
-
-	return &entity, nil
 }
 
 func NewClassRoomRepository(db *gorm.DB) ClassRoomRepository {
