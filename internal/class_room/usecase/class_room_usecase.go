26938022dec125e7bc61d0e1fbb24c565898832e
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-21 23:00:10 +0700

    Complete class_room useCase

--- internal/class_room/usecase/class_room_usecase.go
+++ internal/class_room/usecase/class_room_usecase.go
@@ -1,9 +1,11 @@
 package usecase
 
 import (
+	"errors"
 	"go_online_course/internal/class_room/dto"
 	entity4 "go_online_course/internal/class_room/entity"
 	"go_online_course/internal/class_room/repository"
+	"gorm.io/gorm"
 )
 
 type ClassRoomUseCase interface {
@@ -15,17 +17,36 @@ type ClassRoomUseCaseImpl struct {
 	repository repository.ClassRoomRepository
 }
 
-func (usecase *ClassRoomUseCaseImpl) FindAllByUserID(offset int, limit int, userID int) dto.ClassRoomListResponse {
-	//if err := usecase.repository.FindAllByUserID(offset, limit, userID) != nil {
-	//
-	//}
-	//TODO implement me
-	panic("implement me")
+// Create implements ClassRoomUseCase
+func (useCase *ClassRoomUseCaseImpl) Create(dto dto.ClassRoom) (*entity4.ClassRoom, error) {
+	//	to validation user_id and product_id is available
+	dataClassRoom, err := useCase.repository.FindByOneByUserIDAndProductID(int(dto.UserID), int(dto.ProductID))
+
+	//if data product is empty, skip this condition
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, err
+	}
+
+	if dataClassRoom != nil {
+		return nil, errors.New("you are already purchased this class")
+	}
+
+	classRoom := entity4.ClassRoom{
+		ID:        dto.UserID,
+		ProductID: dto.ProductID,
+	}
+	data, err := useCase.repository.Create(classRoom)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
-func (usecase *ClassRoomUseCaseImpl) Create(dto dto.ClassRoom) (*entity4.ClassRoom, error) {
-	//TODO implement me
-	panic("implement me")
+// FindAllByUserID implements ClassRoomUseCase
+func (useCase *ClassRoomUseCaseImpl) FindAllByUserID(offset int, limit int, userID int) dto.ClassRoomListResponse {
+	classRooms := useCase.repository.FindAllByUserID(offset, limit, userID)
+	classRoomsResp := dto.CreateClassRoomListResponse(classRooms)
+	return classRoomsResp
 }
 
 func NewClassRoomUseCase(repository repository.ClassRoomRepository) ClassRoomUseCase {
